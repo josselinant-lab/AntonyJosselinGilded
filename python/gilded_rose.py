d9b28c912a1394109c1efab8f73751bace76dea5
joantony <Josselin.ANTONY@etu.uca.fr>
2026-03-04 09:51:33 +0100

    Use is_backstage flag inside else block
    Pourquoi : remplace la string brute par le flag booléen.
    Aucun comportement modifié.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -18,7 +18,7 @@ class GildedRose(object):
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if is_backstage:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
